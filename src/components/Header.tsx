059448926070555a6fc9bda71fae3e87f7cdc55f
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-01-01 16:30:36 -0500

    Make navbar responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,74 +1,48 @@
+import { useState } from "react";
+import Navbar from "./Navbar";
+
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="p-16 sticky">
-      <header className="flex justify-between items-center">
-        <div>
-          <h1 className="text-2xl font-bold">Jeremiah Plumstead</h1>
+      <header className="flex flex-col gap-10">
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="text-2xl font-bold">Jeremiah Plumstead</h1>
+          </div>
+          <div className="hidden md:block">
+            <Navbar isResponsive={false} />
+          </div>
+          <div className="md:hidden">
+            <button onClick={toggleNav}>
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                ></path>
+              </svg>
+            </button>
+          </div>
         </div>
-        <nav>
-          <ul className="flex gap-5 font-semibold text-base">
-            <li>
-              <a href="#home"> Home </a>
-            </li>
-            <li>
-              <a href="#experience"> Experience </a>
-            </li>
-            <li>
-              <a href="#projects"> Projects </a>
-            </li>
-            <li>
-              <a href="#education"> Education </a>
-            </li>
-            <li>
-              <a href="#contact"> Contact </a>
-            </li>
-          </ul>
-        </nav>
+        {isOpen && (
+          <div className="flex justify-center">
+            <Navbar isResponsive={true} />
+          </div>
+        )}
       </header>
     </div>
   );
-
-  // <style>
-  // 	.container {
-  // 		padding: 16px 16px 16px 16px;
-  // 		position: sticky;
-  // 	}
-  //
-  // 	.header {
-  // 		display: flex;
-  // 		justify-content: space-between;
-  // 		align-items: center;
-  // 	}
-  //
-  // 	ul {
-  // 		display: flex;
-  // 		gap: 20px;
-  // 	}
-  //
-  // 	li {
-  // 		display: inline;
-  // 		font-weight: 600;
-  // 		font-size: 1.5rem;
-  // 	}
-  //
-  // 	a {
-  // 		text-decoration: none;
-  // 		color: inherit;
-  // 	}
-  //
-  // 	@media (max-width: 768px) {
-  // 		h1 {
-  // 			font-size: 1.5rem;
-  // 		}
-  //
-  // 		ul {
-  // 			display: flex;
-  // 			gap: 5px;
-  // 		}
-  //
-  // 		li {
-  // 			font-size: 0.75rem;
-  // 		}
-  // 	}
-  // </style>
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+export default function Navbar(props: { isResponsive: boolean }) {
+  return (
+    <nav>
+      <ul
+        className={`flex ${props.isResponsive && `flex-col`} ${!props.isResponsive && `gap-5`} font-semibold text-base`}
+      >
+        <li>
+          <a href="#home"> Home </a>
+        </li>
+        <li>
+          <a href="#experience"> Experience </a>
+        </li>
+        <li>
+          <a href="#projects"> Projects </a>
+        </li>
+        <li>
+          <a href="#education"> Education </a>
+        </li>
+        <li>
+          <a href="#contact"> Contact </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
